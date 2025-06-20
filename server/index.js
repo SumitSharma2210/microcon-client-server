50dfa6ca0b55c6bec33c134a042ebc9e6daaade0
SumitSharma2210 <sumit.sharma.gkv@gmail.com>
2025-06-20 11:13:20 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ import express from "express";
 import cors from "cors";
 import testRouter from "./routes/test.js";
 import registerRouter from "./routes/register.js"; // <-- new line
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
--- server/services/supabase.js
+++ server/services/supabase.js
@@ -1,7 +1,11 @@
+// server/services/supabase.js
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://kzimzhnrtrbbjvippewy.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt6aW16aG5ydHJiYmp2aXBwZXd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzMzAyNTIsImV4cCI6MjA2NTkwNjI1Mn0.Uq4Zo4SBqzJ7GsBGltGixMXMFTV041YpnuuNFZY9308";
+import dotenv from "dotenv";
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
